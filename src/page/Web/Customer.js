53983dfefce35751704397be6ea1cd57437efe09
Rizni Mohamed <mnriznimohamed@gmail.com>
2023-02-25 00:35:52 +0530

    shopping page

--- src/page/Web/Customer.js
+++ src/page/Web/Customer.js
@@ -1,44 +1,46 @@
-import { Avatar, Box, Button, IconButton, Typography } from '@mui/material'
-import React from 'react'
-import BreadCrumbs from '../../Components/BreadCrumbs'
+import { Delete, ShoppingBag } from '@mui/icons-material';
+import { Avatar, Box, IconButton } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-import { Add, Delete, Edit, ShoppingBag } from '@mui/icons-material';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import BreadCrumbs from '../../Components/BreadCrumbs';
+import { dialogActions } from '../../Store/dialogSlice';
 
 const Profile = () => {
-
+  const dispatch = useDispatch()
   const navigate = useNavigate()
 
   const loanDetails = [
     {
-      image : "",
+      image: "",
       id: 1,
       name: "default",
-      email : "mnriznimohamed@gmail.com",
+      email: "mnriznimohamed@gmail.com",
       age: 0,
       contact: 15000,
     },
     {
-      image : "",
+      image: "",
       id: 2,
       name: "basic",
-      email : "mnriznimohamed@gmail.com",
+      email: "mnriznimohamed@gmail.com",
       age: 15000,
       contact: 25000,
     },
     {
-      image : "",
+      image: "",
       id: 3,
       name: "medium",
-      email : "mnriznimohamed@gmail.com",
+      email: "mnriznimohamed@gmail.com",
       age: 25000,
       contact: 50000,
     },
     {
-      image : "",
+      image: "",
       id: 4,
       name: "pro",
-      email : "mnriznimohamed@gmail.com",
+      email: "mnriznimohamed@gmail.com",
       age: 50000,
       contact: 100000,
     },
@@ -59,7 +61,7 @@ const Profile = () => {
     { field: 'age', headerName: 'Age', flex: 1, width: 130, headerAlign: "center", align: 'center' },
     { field: 'contact', headerName: 'Contact', flex: 1, width: 130, headerAlign: "center", align: 'center' },
     {
-      field: 'shopHistroy', headerName: 'Shoping Histroy', flex: 1, width: 130, headerAlign: "center", align: 'center',
+      field: 'shopHistroy', headerName: 'Shopping Histroy', flex: 1, width: 130, headerAlign: "center", align: 'center',
       renderCell: (params) => (
         <IconButton size='small' sx={{ color: "#FF8B03", bgcolor: "#3B3B3B !important" }} onClick={() => navigate("shopping history")} >
           <ShoppingBag fontSize='small' sx={{ color: "#FF8B03 !important" }} />
@@ -69,12 +71,24 @@ const Profile = () => {
     {
       field: 'delete', headerName: 'Delete', flex: 1, width: 130, headerAlign: "center", align: 'center',
       renderCell: (params) => (
-        <IconButton size='small' sx={{ color: "red", bgcolor: "#3B3B3B !important" }}>
+        <IconButton onClick={() => handleDelete(params.row)} size='small' sx={{ color: "red", bgcolor: "#3B3B3B !important" }}>
           <Delete fontSize='small' sx={{ color: "red !important" }} />
         </IconButton>
       )
     },
   ];
+
+  const handleDelete = row => {
+    dispatch(dialogActions.show([
+      "delete",
+      (formData) => {
+        alert("Deleted " + row.id)
+      },
+      "Are you sure do you want to delete this customer? All purchaes assoicated with this customer will also deleted"
+    ]))
+    console.log(row);
+  }
+
 
 
   return (
--- src/page/Web/ShoppingHistory.js
+++ src/page/Web/ShoppingHistory.js
@@ -1,74 +1,108 @@
-import { Avatar, Box, Button, IconButton, Typography } from '@mui/material'
+import { Avatar, Box, Button, IconButton, Slider, Typography } from '@mui/material'
 import React from 'react'
 import BreadCrumbs from '../../Components/BreadCrumbs'
 import { DataGrid } from '@mui/x-data-grid';
 import { Add, Delete, Edit, Paid, Receipt, ShoppingBag } from '@mui/icons-material';
+import { useDispatch } from 'react-redux';
+import { dialogActions } from '../../Store/dialogSlice';
+import defaultProduct from '../../LocalData/image/default_product.png'
 
 const ShoppingHistory = () => {
+  const dispatch = useDispatch()
 
-  const loanDetails = [
+  const ShoppingHistroy = [
     {
-      image: "",
+      image: defaultProduct,
       id: 1,
       name: "default",
-      installemt: "2/3",
+      installemt_total: 3,
+      installment_paid: 2,
+      product_price: 12554,
+      paid: 4556,
     },
     {
-      image: "",
+      image: defaultProduct,
       id: 2,
       name: "basic",
-      installemt: "2/3",
+      installemt_total: 3,
+      installment_paid: 3,
+      product_price: 12554,
+      paid: 4556,
     },
     {
-      image: "",
+      image: defaultProduct,
       id: 3,
       name: "medium",
-      installemt: "2/5",
+      installemt_total: 3,
+      installment_paid: 1,
+      product_price: 12554,
+      paid: 4556,
     },
     {
-      image: "",
+      image: defaultProduct,
       id: 4,
       name: "pro",
-      installemt: "2/3",
+      installemt_total: 4,
+      installment_paid: 2,
+      product_price: 12554,
+      paid: 4556,
     },
   ]
 
   const columns = [
     {
-      field: 'iamge', headerName: 'Image', flex: 1, width: 130, headerAlign: "center", align: 'center',
+      field: 'image', headerName: 'Product Image', flex: 1, width: 130, headerAlign: "center", align: 'center',
       renderCell: ({ row: { image } }) => (
-        <Avatar src={image} variant="rounded" sx={{ bgcolor: "#3B3B3B" }} />
+        <Avatar src={image} variant="square" sx={{ bgcolor: "#3B3B3B", borderRadius:0.2 }} />
       )
     },
-    { field: 'id', headerName: 'ID', flex: 1, width: 130, headerAlign: "center", align: 'center' },
-    { field: 'name', headerName: 'Name', flex: 1, width: 130, headerAlign: "center", align: 'center' },
-    { field: 'installemt', headerName: 'Installemt', flex: 1, width: 130, headerAlign: "center", align: 'center' },
+    { field: 'id', headerName: 'Invoice ID', flex: 1, width: 130, headerAlign: "center", align: 'center' },
+    { field: 'name', headerName: 'Product Name', flex: 1, width: 130, headerAlign: "center", align: 'center' },
+    { field: 'product_price', headerName: 'Product Price', flex: 1, width: 130, headerAlign: "center", align: 'center' },
+    { field: 'paid', headerName: 'Paid Amount', flex: 1, width: 130, headerAlign: "center", align: 'center' },
     {
-      field: 'loan', headerName: 'Loan', flex: 1, width: 130, headerAlign: "center", align: 'center',
-      renderCell: (params) => (
-        <IconButton size='small' sx={{ color: "#FF8B03", bgcolor: "#3B3B3B !important" }}>
-          <Paid fontSize='small' sx={{ color: "#FF8B03 !important" }} />
-        </IconButton>
+      field: 'installemt', headerName: 'Installemt', flex: 1, width: 130, headerAlign: "center", align: 'center',
+      renderCell: ({ row: { installemt_total, installment_paid } }) => (
+        <Slider
+          aria-label="Always visible"
+          defaultValue={installment_paid}
+          step={1}
+          size="small"
+          marks
+          max={installemt_total}
+          disabled={true}
+          sx={{
+            "	.Mui-disabled": { color: "primary.main" },
+            ".MuiSlider-track": { color: "primary.main" },
+            ".MuiSlider-rail": { color: "primary.main" },
+            ".MuiSlider-mark": { color: "black" },
+          }}
+        />
       )
     },
-    {
-      field: 'Payment', headerName: 'Payment', flex: 1, width: 130, headerAlign: "center", align: 'center',
-      renderCell: (params) => (
-        <IconButton size='small' sx={{ color: "#FF8B03", bgcolor: "#3B3B3B !important" }}>
-          <Receipt fontSize='small' sx={{ color: "#FF8B03 !important" }} />
-        </IconButton>
-      )
-    },
-
     {
       field: 'delete', headerName: 'Delete', flex: 1, width: 130, headerAlign: "center", align: 'center',
       renderCell: (params) => (
-        <IconButton size='small' sx={{ color: "red", bgcolor: "#3B3B3B !important" }}>
+        <IconButton onClick={() => handleDelete(params.row)} size='small' sx={{ color: "red", bgcolor: "#3B3B3B !important" }}>
           <Delete fontSize='small' sx={{ color: "red !important" }} />
         </IconButton>
       )
     },
   ];
+
+
+
+  const handleDelete = row => {
+    dispatch(dialogActions.show([
+      "delete",
+      (formData) => {
+        alert("Deleted " + row.id)
+      },
+      "Are you sure do you want to delete this invoice? This cannot be undone"
+    ]))
+    console.log(row);
+  }
+
 
 
   return (
@@ -77,7 +111,7 @@ const ShoppingHistory = () => {
 
       <Box mt={2}>
         <DataGrid
-          rows={loanDetails}
+          rows={ShoppingHistroy}
           columns={columns}
           autoHeight={true}
           hideFooter={true}
